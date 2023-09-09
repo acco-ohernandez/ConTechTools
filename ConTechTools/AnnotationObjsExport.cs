f4fe155232c341cf54fb9a828dd4f278b4e583d9
Orlando R Hernandez <47466479+acco-ohernandez@users.noreply.github.com>
2023-09-08 19:20:04 -0700

    Starting new branch to work on the Annotation Objects Export

---
+++ ConTechTools/AnnotationObjsExport.cs
@@ -0,0 +1,42 @@
+#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+#endregion
+
+namespace ConTechTools
+{
+    [Transaction(TransactionMode.Manual)]
+    public class AnnotationObjsExport : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+
+
+            // Modify document within a transaction
+
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("Transaction Name");
+                tx.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
