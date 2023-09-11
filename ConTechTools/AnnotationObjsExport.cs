8561a2d10a692bc41739d12c9b7a5f3165896b9b
Orlando R Hernandez <47466479+acco-ohernandez@users.noreply.github.com>
2023-09-11 01:56:00 -0700

    working on the output

--- ConTechTools/AnnotationObjsExport.cs
+++ ConTechTools/AnnotationObjsExport.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 
 using Autodesk.Revit.ApplicationServices;
@@ -9,8 +10,6 @@ using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
-
-
 #endregion
 
 namespace ConTechTools
@@ -19,41 +18,51 @@ namespace ConTechTools
     public class AnnotationObjsExport : IExternalCommand
     {
         public Result Execute(
-    ExternalCommandData commandData,
-    ref string message,
-    ElementSet elements)
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
         {
-            UIApplication uiapp = commandData.Application;
-            UIDocument uidoc = uiapp.ActiveUIDocument;
-            Application app = uiapp.Application;
-            Document doc = uidoc.Document;
-
-            // Show a message to confirm that the command is executing
-            TaskDialog.Show("Annotation Object Styles Export", "Command is executing.");
-
-            // Get all annotation categories
-            List<Category> annotationCategories = GetAnnotationCategories(doc);
-
-            if (annotationCategories != null && annotationCategories.Any())
+            try
             {
-                // Export annotation object styles for each category
-                foreach (Category category in annotationCategories)
+                UIApplication uiapp = commandData.Application;
+                UIDocument uidoc = uiapp.ActiveUIDocument;
+                Application app = uiapp.Application;
+                Document doc = uidoc.Document;
+
+                // Show a message to confirm that the command is executing
+                TaskDialog.Show("Annotation Object Styles Export", "Command is executing.");
+
+                // Get all annotation categories
+                List<Category> annotationCategories = GetAnnotationCategories(doc);
+
+                if (annotationCategories != null && annotationCategories.Any())
                 {
-                    // Export object styles for this category
-                    // ExportObjectStyles(doc, category);
-                    Debug.Print($"{category.CategoryType} - {category.Name} - {category.IsReadOnly} - {category.LineColor}");
-                }
-            }
-            else
-            {
-                TaskDialog.Show("No Annotation Categories", "There are no annotation categories in the document.");
-            }
+                    // Export annotation object styles for each category, including sub-settings
+                    foreach (Category category in annotationCategories)
+                    {
+                        Debug.Print($"{category.Name} - {category.IsReadOnly} - {category.LineColor}");
+                        ExportObjectStyles(doc, category);
 
-            // Return a success result
-            return Result.Succeeded;
+                        // Export sub-settings for this category
+                        ExportSubSettings(doc, category);
+                    }
+                }
+                else
+                {
+                    TaskDialog.Show("No Annotation Categories", "There are no annotation categories in the document.");
+                }
+
+                // Return a success result
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
         }
 
-        // Helper method to get all annotation categories
+        // Helper method to get all annotation categories, including sub-settings
         private List<Category> GetAnnotationCategories(Document doc)
         {
             Categories categories = doc.Settings.Categories;
@@ -75,35 +84,77 @@ namespace ConTechTools
             return annotationCategories;
         }
 
-        private List<Category> GetAnnotationCategories2(Document doc)
+        // Helper method to export sub-settings of a category
+        // ...
+
+        // Helper method to export sub-settings of a category
+        private void ExportSubSettings(Document doc, Category category)
         {
-            Categories categories = doc.Settings.Categories;
+            CategoryNameMap subCategories = category.SubCategories;
 
-            List<Category> annotationCategories = new List<Category>();
-
-            // Use the BuiltInCategory enumeration to filter annotation categories
-            foreach (Category cat in categories)
+            if (subCategories != null && subCategories.Size > 0)
             {
-                if (cat.CategoryType == CategoryType.Annotation)
+                // Sort the subCategories by name
+                List<Category> sortedSubCategories = subCategories.Cast<Category>()
+                    .OrderBy(subCat => subCat.Name).ToList();
+
+                foreach (Category subCategory in sortedSubCategories)
                 {
-                    annotationCategories.Add(cat);
+                    Debug.Print($"|-- {subCategory.Name} - {subCategory.IsReadOnly} - {subCategory.LineColor}");
+                    ExportObjectStyles(doc, subCategory);
+
+                    // Recursively export sub-settings of this sub-category
+                    ExportSubSettings(doc, subCategory);
                 }
             }
-
-            // Sort the annotationCategories by category name in ascending order
-            //annotationCategories = annotationCategories.OrderBy(cat => cat.Name).ToList();
-
-            return annotationCategories;
         }
 
 
         // Helper method to export object styles for a category
+        // Helper method to export object styles for a category
         private void ExportObjectStyles(Document doc, Category category)
         {
-            // Implement your code to export object styles for the given category here
-            // You can use the category variable to identify the category and export styles accordingly
-        }
+            // Check if the Desktop folder exists
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (!Directory.Exists(desktopPath))
+            {
+                Debug.Print("Desktop folder not found.");
+                return;
+            }
 
+            // Define the CSV file path
+            string csvFilePath = Path.Combine(desktopPath, "AnnotationObjectStyles.csv");
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(csvFilePath, true))
+                {
+                    // Write category information to the CSV file
+                    writer.Write($"Category Name: {category.Name},");
+                    writer.Write($"Is Read-Only: {category.IsReadOnly},");
+                    writer.Write($"Line Color: {category.LineColor}");
+                    writer.WriteLine(); // Add an empty line for separation
+
+                    // Implement your code here to export specific object styles for the given category
+                    // Example: You can iterate through the object styles and write them to the CSV file.
+                    // Replace the following code with your actual implementation.
+
+                    // writer.WriteLine("Object Style 1: Some Details");
+                    // writer.WriteLine("Object Style 2: Some Details");
+                    // ...
+
+                    // End of object styles
+
+                    //writer.WriteLine(); // Add an empty line for separation
+                }
+
+                Debug.Print($"Object styles for '{category.Name}' exported to '{csvFilePath}'.");
+            }
+            catch (Exception ex)
+            {
+                Debug.Print($"Error exporting object styles for '{category.Name}': {ex.Message}");
+            }
+        }
 
     }
 }
